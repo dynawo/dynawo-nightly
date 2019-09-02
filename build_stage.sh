774fe7259feba8eaa1344aabc1293a9fea0ea615
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-02 16:21:56 +0200

    Added an url variable.

--- build_stage.sh
+++ build_stage.sh
@@ -4,7 +4,9 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$COMMAND"
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$NRT_COMMAND"
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  curl -v -L -H "Authorization: token $GITHUB_TOKEN" $(curl -s -L -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/dynawo/dynawo/releases/latest | grep "Dynawo_MacOS" | grep url | cut -d '"' -f 4) -o $HOME/Dynawo_MacOS_latest.zip
+  zip_url=$(curl -s -L -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/dynawo/dynawo/releases/latest | grep "Dynawo_MacOS" | grep url | cut -d '"' -f 4)
+  echo $zip_url
+  curl -v -L -H "Authorization: token $GITHUB_TOKEN" $zip_url -o $HOME/Dynawo_MacOS_latest.zip
   unzip $HOME/Dynawo_MacOS_latest.zip -d $HOME/Dynawo_MacOS_latest
   cd dynawo
   ls $HOME/Dynawo_MacOS_latest/lib
