81b91628fd25775f98125567ac2a18e703312b15
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-02 15:36:29 +0200

    Changed /opt to HOME to avoid right errors.

--- build_stage.sh
+++ build_stage.sh
@@ -4,14 +4,11 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$COMMAND"
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$NRT_COMMAND"
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  zip_file=$(curl -s -L -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/dynawo/dynawo/releases/latest | grep "Dynawo_MacOS" | grep url | cut -d '"' -f 4)
-  curl -s -L -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/dynawo/dynawo/releases/latest
-  echo zip_file $zip_file
-  (cd /opt && curl -L -H "Authorization: token $GITHUB_TOKEN" $zip_file -o Dynawo_MacOS_latest.zip)
-  unzip /opt/Dynawo_MacOS_latest.zip -d /opt/Dynawo_MacOS_latest
+  curl -L -H "Authorization: token $GITHUB_TOKEN" $(curl -s -L -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/repos/dynawo/dynawo/releases/latest | grep "Dynawo_MacOS" | grep url | cut -d '"' -f 4) -o $HOME/Dynawo_MacOS_latest.zip
+  unzip $HOME/Dynawo_MacOS_latest.zip -d $HOME/Dynawo_MacOS_latest
   cd dynawo
-  ls /opt/Dynawo_MacOS_latest/lib
-  ls /opt/Dynawo_MacOS_latest/include
+  ls $HOME/Dynawo_MacOS_latest/lib
+  ls $HOME/Dynawo_MacOS_latest/include
   util/envDynawo.sh build-3rd-party-version || { echo "Error with build-3rd-party-version."; exit 1; }
   util/envDynawo.sh build-dynawo || { echo "Error with build-dynawo."; exit 1; }
 fi
--- define_variables.sh
+++ define_variables.sh
@@ -28,10 +28,10 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   echo BUILD_OMC_COMMAND $BUILD_OMC_COMMAND
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
   export DYNAWO_HOME=$(pwd)/dynawo
-  export DYNAWO_INSTALL_OPENMODELICA=/opt/Dynawo_MacOS_latest/OpenModelica
-  export DYNAWO_SRC_OPENMODELICA=/opt/OpenModelica/Source
+  export DYNAWO_INSTALL_OPENMODELICA=$HOME/Dynawo_MacOS_latest/OpenModelica
+  export DYNAWO_SRC_OPENMODELICA=$DYNAWO_HOME/OpenModelica/Source
   export DYNAWO_NB_PROCESSORS_USED=$(sysctl hw | grep ncpu | awk '{print $(NF)}')
   export DYNAWO_RESULTS_SHOW="false"
-  export DYNAWO_LIBARCHIVE_HOME=/opt/Dynawo_MacOS_latest
-  export DYNAWO_BOOST_HOME=/opt/Dynawo_MacOS_latest
+  export DYNAWO_LIBARCHIVE_HOME=$HOME/Dynawo_MacOS_latest
+  export DYNAWO_BOOST_HOME=$HOME/Dynawo_MacOS_latest
 fi
