aec1b3caf5cc357781329d1f878318026ab8ccde
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-03 14:11:07 +0200

    Try debug only to debug seg fault.

--- build_stage.sh
+++ build_stage.sh
@@ -7,10 +7,10 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
   cd dynawo
   util/envDynawo.sh build-3rd-party-version || { echo "Error with build-3rd-party-version."; exit 1; }
   util/envDynawo.sh build-dynawo || { echo "Error with build-dynawo."; exit 1; }
+  # if [ "$DYNAWO_BUILD_TYPE" = "Debug" ]; then
+  #   util/envDynawo.sh build-tests || { echo "Error with build-tests."; exit 1; }
+  # fi
   if [ "$DYNAWO_BUILD_TYPE" = "Debug" ]; then
-    util/envDynawo.sh build-tests || { echo "Error with build-tests."; exit 1; }
-  fi
-  if [ "$DYNAWO_BUILD_TYPE" = "Release" ]; then
     util/envDynawo.sh jobs nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs || echo "Error"
     cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
     cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawoCompiler.log
