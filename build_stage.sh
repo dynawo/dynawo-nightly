0e158b9d182305d3327af07a0670b0bf5ee72d9e
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-04 12:11:48 +0200

    Added new lldb debug trace.

--- build_stage.sh
+++ build_stage.sh
@@ -15,6 +15,20 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "run" > ~/cmd.gdb
     echo "bt" >> ~/cmd.gdb
     echo "quit" >> ~/cmd.gdb
+    util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs | tee ~/backtrace
+    sed -n -e '(lldb) bt/,$p' ~/backtrace | grep frame | grep -o "at .*" | cut -d ' ' -f 2 | sed 's/:[0-9]*$//' > ~/breakpoints
+    sed 's/^/b /g' ~/breakpoints > ~/breakpoints.gdb
+    N=$(wc -l ~/breakpoints.gdb | awk '{print $1}')
+    echo "run" >> ~/breakpoints.gdb
+    for i in `seq 1 $N`; do
+      echo "frame variable" >> ~/breakpoints.gdb
+      echo "fr v" >> ~/breakpoints.gdb
+      echo "continue" >> ~/breakpoints.gdb
+    done
+    echo "quit" >> ~/breakpoints.gdb
+    cat ~/breakpoints.gdb
+    sed -i '' 's/cmd.gdb/breakpoints.gdb/' /Users/travis/build/dynawo/dynawo-nightly/dynawo/install/clang4.2.1/master/Debug-cxx11/shared/dynawo/bin/launcher
+    cat /Users/travis/build/dynawo/dynawo-nightly/dynawo/install/clang4.2.1/master/Debug-cxx11/shared/dynawo/bin/launcher
     util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs
     # tail nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
     # tail -50 nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawoCompiler.log
