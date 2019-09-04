a53d22b3a4fe6427d1b95f8b3ad8c80f324c133c
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-04 19:49:10 +0200

    Added other test.

--- build_stage.sh
+++ build_stage.sh
@@ -18,6 +18,7 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "fr v" >> ~/cmd.gdb
     echo "quit" >> ~/cmd.gdb
     util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs | tee ~/backtrace
+    util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs | tee ~/backtrace2
     ls nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/compilation
     sed -n -e '/(lldb) bt/,$p' ~/backtrace | grep frame | grep -o "at .*" | cut -d ' ' -f 2 | sed 's/:[0-9]*$//' > ~/breakpoints
     sed 's/^/b /g' ~/breakpoints > ~/breakpoints.gdb
@@ -31,7 +32,6 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "quit" >> ~/breakpoints.gdb
     cat ~/breakpoints.gdb
     sed -i '' 's/cmd.gdb/breakpoints.gdb/' /Users/travis/build/dynawo/dynawo-nightly/dynawo/install/clang4.2.1/master/Debug-cxx11/shared/dynawo/bin/launcher
-    cat /Users/travis/build/dynawo/dynawo-nightly/dynawo/install/clang4.2.1/master/Debug-cxx11/shared/dynawo/bin/launcher
     util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs
     # tail nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
     # tail -50 nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawoCompiler.log
@@ -39,5 +39,9 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # util/envDynawo.sh jobs nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo "Error"
     # tail nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/outputs/logs/dynawo.log
     # util/envDynawo.sh nrt || { echo "Error with nrt."; exit 1; }
+    $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
+    echo
+    $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs $HOME/Dynawo_MacOS_latest/sources/nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
+    echo
   fi
 fi
