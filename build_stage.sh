29ec3e0df1eb1d7fb7e59961ccc5509d5fb08f38
Gautier Bureau <gautier.bureau@gmail.com>
2019-09-05 16:57:39 +0200

    new test

--- build_stage.sh
+++ build_stage.sh
@@ -22,17 +22,30 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "frame variable" >> ~/cmd.gdb
     echo "fr v" >> ~/cmd.gdb
     echo "quit" >> ~/cmd.gdb
-    util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs 2>&1 | tee ~/backtrace
-    cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
-    util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs
-    cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
+    #util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs 2>&1 | tee ~/backtrace
+    #cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
+    #util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs
+    #cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
+    echo "==============================================================="
+    echo "ENV"
+    env
+    echo "==============================================================="
     util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs > ~/backtrace
-    util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs 2>&1 | tee ~/backtrace2
+    echo "==============================================================="
+    echo "cat dynawo.log"
+    cat nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/logs/dynawo.log
+    echo "==============================================================="
+    #util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs 2>&1 | tee ~/backtrace2
     util/envDynawo.sh dump-model nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/compilation/GEN____1_SM.dylib -o ~/dump.xml
+    echo "==============================================================="
+    echo "cat dump.xml"
     cat ~/dump.xml
+    echo "==============================================================="
     #ls nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/outputs/compilation
+    echo "==============================================================="
     echo "cat backtrace"
     cat ~/backtrace
+    echo "==============================================================="
     sed -n -e '/(lldb) bt/,$p' ~/backtrace | grep frame | grep -o "at .*" | cut -d ' ' -f 2 | sed 's/:[0-9]*$//' > ~/breakpoints
     sed -i '' '/main.cpp/d' ~/breakpoints
     #echo "cat breakpoints"
@@ -53,6 +66,10 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     #echo "run" >> ~/breakpoints.gdb
     #echo "continue" >> ~/breakpoints.gdb
     echo "quit" >> ~/breakpoints.gdb
+    echo "==============================================================="
+    echo "cat breakpoints.gdb"
+    cat ~/breakpoints.gdb
+    echo "==============================================================="
     #cat ~/breakpoints.gdb
     sed -i '' 's/cmd.gdb/breakpoints.gdb/' /Users/travis/build/dynawo/dynawo-nightly/dynawo/install/clang4.2.1/master/Debug-cxx11/shared/dynawo/bin/launcher
     util/envDynawo.sh jobs-gdb nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs
@@ -62,11 +79,11 @@ elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     # util/envDynawo.sh jobs nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo "Error"
     # tail nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/outputs/logs/dynawo.log
     # util/envDynawo.sh nrt || { echo "Error with nrt."; exit 1; }
-    $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
-    echo
-    $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs $HOME/Dynawo_MacOS_latest/sources/nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
-    echo
-    $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs || echo
-    echo
+    # $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
+    # echo
+    # $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs $HOME/Dynawo_MacOS_latest/sources/nrt/data/IEEE14/IEEE14_BasicTestCases/IEEE14_DisconnectGroup/IEEE14.jobs || echo
+    # echo
+    # $HOME/Dynawo_MacOS_latest/bin/execDynawo.sh jobs nrt/data/IEEE14/IEEE14_SyntaxExamples/IEEE14_ModelicaModel/IEEE14.jobs || echo
+    # echo
   fi
 fi
