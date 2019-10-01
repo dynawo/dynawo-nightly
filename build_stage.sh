322aa7a7b0bf722e2e71940e945b1d746cc25a5e
Gautier Bureau <gautier.bureau@gmail.com>
2019-10-01 10:00:58 +0200

    Tests are only done in shared for the moment.

--- build_stage.sh
+++ build_stage.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$COMMAND"
   docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$NRT_COMMAND"
-  if [ "$DYNAWO_BUILD_TYPE" = "Debug" ]; then
+  if [ "$DYNAWO_BUILD_TYPE" = "Debug" -a "$DYNAWO_LIBRARY_TYPE" = "SHARED" ]; then
     docker exec $dynawo_env $dynawo_om_env $dynawo_env_url dynawo_travis_container bash -c "$TESTS_COMMAND"
   fi
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
