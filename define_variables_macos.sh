c81e99344273f04356b9b6cac22c3b471773e285
Gautier Bureau <gautier.bureau@gmail.com>
2019-11-21 23:32:49 +0100

    Corrected mistakes in travis config file.

--- define_variables_macos.sh
+++ define_variables_macos.sh
@@ -5,3 +5,5 @@ export DYNAWO_HOME=$(pwd)/dynawo
 export DYNAWO_INSTALL_OPENMODELICA=$HOME/Dynawo_MacOS_latest/OpenModelica
 export DYNAWO_SRC_OPENMODELICA=$DYNAWO_HOME/OpenModelica/Source
 export DYNAWO_NB_PROCESSORS_USED=$(sysctl hw | grep ncpu | awk '{print $(NF)}')
+
+export DYNAWO_BOOST_HOME=$HOME/Dynawo_MacOS_latest
--- install_stage_macos.sh
+++ install_stage_macos.sh
@@ -9,5 +9,3 @@ unzip -q $HOME/Dynawo_MacOS_latest.zip -d $HOME/Dynawo_MacOS_latest
 (cd $HOME/Dynawo_MacOS_latest/include;rm -rf CRV* CSTR* DYD* DYN* EXTVAR* JOB* PAR* TL* FS* IIDM SuiteSparse_config.h adept adept.h adept_arrays.h amd.h btf.h colamd.h config.h gtest_dynawo.h ida kinsol klu.h libzip nvector sundials sunlinsol sunmatrix sunnonlinsol xercesc xml zconf.h zlib.h)
 (cd $HOME/Dynawo_MacOS_latest/lib;rm -f libdynawo* libiidm* libsundials* libxerces-c* libzip* libklu* libamd* libcolamd* libbtf* libXML* libadept* libz* libsuitesparseconfig*)
 pip install lxml psutil
-echo "[install]
-prefix=" > ~/.pydistutils.cfg
