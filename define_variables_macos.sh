e9ed9cecf8377f4df4a1b0d7afa92460a7a05661
Gautier Bureau <gautier.bureau@gmail.com>
2019-12-17 09:38:52 +0100

    #6 Deactivate temporarily macos build that are not working. Some additional fixes.

--- define_variables_macos.sh
+++ define_variables_macos.sh
@@ -6,6 +6,5 @@ export DYNAWO_INSTALL_OPENMODELICA=$HOME/Dynawo_MacOS_latest/OpenModelica
 export DYNAWO_SRC_OPENMODELICA=$DYNAWO_HOME/OpenModelica/Source
 export DYNAWO_NB_PROCESSORS_USED=$(sysctl hw | grep ncpu | awk '{print $(NF)}')
 
-export DYNAWO_BOOST_HOME=$HOME/Dynawo_MacOS_latest
-
 export MACOSX_DEPLOYMENT_TARGET=10.13
+export DYNAWO_BOOST_HOME_DEFAULT=false
