c1b523b0779f82d46080da5dee0b18041cd019c1
Gautier Bureau <gautier.bureau@gmail.com>
2020-04-21 09:33:12 +0200

    #16 Adapt python server with python3 in Bionic.

--- install_stage_bionic.sh
+++ install_stage_bionic.sh
@@ -2,5 +2,5 @@
 
 docker run -itd -u dynawo_travis --name dynawo_travis_container dynawo/dynawo-travis-nightly-bionic
 docker exec dynawo_travis_container bash -c "$GIT_COMMAND"
-docker exec dynawo_travis_container bash -c "cd /opt;nohup python -m SimpleHTTPServer 8080 &> /dev/null &"
+docker exec dynawo_travis_container bash -c "cd /opt;nohup python3 -m http.server 8080 &> /dev/null &"
 docker exec dynawo_travis_container bash -c "$LOG_COMMAND"
