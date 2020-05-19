1f942a0161b88aa4061c33f1e9c1e970ef05243f
Gautier Bureau <gautier.bureau@gmail.com>
2020-05-19 23:28:20 +0200

    Force sequential build for macos.

--- define_variables_macos.sh
+++ define_variables_macos.sh
@@ -4,7 +4,8 @@ export DYNAWO_RESULTS_SHOW="false"
 export DYNAWO_HOME=$(pwd)/dynawo
 export DYNAWO_INSTALL_OPENMODELICA=$HOME/Dynawo_MacOS_latest/OpenModelica
 export DYNAWO_SRC_OPENMODELICA=$DYNAWO_HOME/OpenModelica/Source
-export DYNAWO_NB_PROCESSORS_USED=$(sysctl hw | grep ncpu | awk '{print $(NF)}')
+# export DYNAWO_NB_PROCESSORS_USED=$(sysctl hw | grep ncpu | awk '{print $(NF)}')
+export DYNAWO_NB_PROCESSORS_USED=1
 
 export MACOSX_DEPLOYMENT_TARGET=10.14
 export DYNAWO_BOOST_HOME_DEFAULT=false
